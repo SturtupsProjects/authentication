fa7253969e31fea8a1cb11df97fdfe7d32419c75
Jons <ozodjon160605@gmail.com>
2024-12-25 17:55:14 +0500

    ok

--- migrations/000001_auth_service_table.up.sql
+++ migrations/000001_auth_service_table.up.sql
@@ -20,14 +20,16 @@ CREATE TABLE users
     company_id   UUID REFERENCES company (company_id),
     created_at   TIMESTAMP DEFAULT NOW()
 );
-create type type as enum ('client', 'suplier');
+CREATE TYPE client_type AS ENUM ('mijoz', 'ta''minotchi');
+
 CREATE TABLE clients
 (
-    id         UUID      DEFAULT gen_random_uuid() PRIMARY KEY,
-    full_name  VARCHAR(60) NOT NULL,
-    type       type        NOT NULL,
-    address    VARCHAR(50),
-    phone      VARCHAR(13),
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    full_name VARCHAR(60) NOT NULL,
+    type client_type NOT NULL,
+    address VARCHAR(50),
+    phone VARCHAR(13),
     created_at TIMESTAMP DEFAULT NOW()
 );
+
 
